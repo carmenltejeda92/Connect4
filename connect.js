45564524b67c3740ff2cdf0704a7fec227a444d4
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-11-07 14:23:57 -0500

    Added divs and style elements

--- connect.js
+++ connect.js
@@ -1,3 +1,53 @@
 /*  Author: Carmen Tejeda
     Date: 11/07/2023
     Purpose: Creating a connect 4 game for two players.  */
+
+const body = document.querySelector('#body')
+const header = document.querySelector('#connect')
+const section = document.querySelector('#playerOne')
+const main = document.querySelector('#gameBoard')
+const aside = document.querySelector('#playerTwo')
+const footer = document.querySelector('.bottomButtons')
+
+
+header.style.height = '50px'
+header.style.width = '150px'
+header.style.backgroundColor = 'white'
+header.style.border = '1px solid black'
+header.style.margin = 'auto'
+header.style.textAlign = 'center'
+header.style.verticalAlign = 'center'
+
+
+section.style.height = '300px'
+section.style.width = '150px'
+section.style.backgroundColor = 'white'
+section.style.border = '1px solid black'
+section.style.textAlign = 'center'
+
+
+aside.style.height = '300px'
+aside.style.width = '150px'
+aside.style.backgroundColor = 'white'
+aside.style.border = '1px solid black'
+aside.style.textAlign = 'center'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
