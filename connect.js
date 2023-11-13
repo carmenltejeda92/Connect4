f3b712d443bbfc0b4c24ca1d475d9edd015c4126
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-11-12 22:59:44 -0500

    Page still functional w/changes made

--- connect.js
+++ connect.js
@@ -3,19 +3,16 @@
     Purpose: Creating a connect 4 game for two players.  */
 
 // const body = document.querySelector('#body')
-// const header = document.querySelector('#connect')
 const section = document.querySelector('#playerOne')
 const main = document.querySelector('#gameBoard')
 const aside = document.querySelector('#playerTwo')
 const footer = document.querySelector('.bottomButtons')
 const endGame = document.querySelector('#endGame')
-const input1 = document.createElement('input')
-const input2 = document.createElement('input')
+const input1 = document.querySelector('#input1')
+const input2 = document.querySelector('#input2')
 const br = document.createElement('br')
 const br2 = document.createElement('br')
-// const boxes = document.createElement('div')
-let columns = 7
-let rows = 6
+
 let gameOver = true
 const player1 = document.createElement('div')
 let player2
@@ -23,20 +20,10 @@ let p1
 let p2
 
 section.innerHTML = "Player 1"
-input1.style.width = "75px"
 section.append(br)
 section.appendChild(input1)
 submit.innerText = 'Submit'
 section.appendChild(submit)
-
-// main.style.height = '550px'
-// main.style.width = '698px'
-// // main.style.backgroundColor = 'blue'
-// main.style.border = '3px solid black'
-// main.style.margin ='35px'
-// main.style.textAlign = 'center'
-// main.style.flexWrap = 'wrap'
-// main.style.display = 'flex'
 
 aside.innerHTML = "Player 2"
 input2.style.width = '75px'
@@ -59,31 +46,6 @@ submit.addEventListener('click', () =>{
     console.log(player1)
 })
 
-
-// window.onload = function() {
-//     gameBoard = []
-
-//     for(let i=0; i<rows; i++){
-//         let rowArr = []
-//         for(let t=0; t<columns; t++){
-//             rowArr.push(' ')
-
-//             const boxes = document.createElement('div')
-//             boxes.id = i.toString() + "-" + t.toString()
-//             boxes.classList.add("boxes")
-//             boxes.style.width = '90px'
-//             boxes.style.height = '89px'
-//             boxes.style.border = '1px solid black'
-//             boxes.style.backgroundColor = 'white'
-//             boxes.style.margin = 'auto'
-//             boxes.style.borderRadius = '50%'
-//             boxes.className = 'boxes'
-//             boxes.addEventListener('click', turn())
-//             main.append(boxes)
-//         }
-//         gameBoard.push(rowArr)
-//     }
-// }
 
 function turn(){
     if(gameOver) return
